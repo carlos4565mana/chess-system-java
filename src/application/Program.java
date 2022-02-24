a1d50ea88dd8976a90c959621fab0ac62e6fc409
carlos4565mana <carloscal61@gmail.com>
2022-02-24 14:48:50 -0300

    Fix better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,8 +39,13 @@ public class Program {
 					captured.add(capturedPiece);
 				}
 				if(chessMatch.getPromoted() != null) {
-					System.out.print("Enter piece for promotion (B/N/R/Q): =>");
+					System.out.print("Enter piece for promotion (B/N/R/Q): => ");
 					String type = sc.nextLine();
+					while(!type.equals("B") && type.equals("N") && !type.equals("R") && type.equals("Q")){
+						System.out.print("Enter piece for promotion (B/N/R/Q): => ");
+						type = sc.nextLine().toUpperCase();
+						
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 
